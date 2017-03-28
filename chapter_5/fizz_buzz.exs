a7377186e9c923461f20d2c832abea4a4214ac45
Steven Warren <steven@stevencwarren.com>
2017-03-27 22:26:27 -0500

    Fixes Rem fizz buzz

--- chapter_5/fizz_buzz.exs
+++ chapter_5/fizz_buzz.exs
@@ -1,10 +1,20 @@
 fizz = fn
-  { 0, 0, _ } -> 'FizzBuzz'
-  { 0, _, _ } -> 'Fizz'
-  { _, 0, _ } -> 'Fizz'
+  { 0, 0, _ } -> "FizzBuzz"
+  { 0, _, _ } -> "Fizz"
+  { _, 0, _ } -> "buzz"
+  { _, _, c } -> c
 end
 
-IO.puts fizz.({ 0, 0, 'baz' })
-IO.puts fizz.({ 0, 'foo', 'baz' })
-IO.puts fizz.({ 'foo', 0, 'baz' })
-IO.puts fizz.({ 'foo', 'bar', 'baz' })
+remFizz = fn (n) ->
+  arg1 = rem(n,3)
+  arg2 = rem(n,5)
+  fizz.({ arg1, arg2, n })
+end
+
+IO.puts remFizz.(10)
+IO.puts remFizz.(11)
+IO.puts remFizz.(12)
+IO.puts remFizz.(13)
+IO.puts remFizz.(14)
+IO.puts remFizz.(15)
+IO.puts remFizz.(16)
